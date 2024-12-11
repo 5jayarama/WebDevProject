51d1235a2f4c209e64b43cc1754fcd2d5aa8452e
Xinning Liu <147454675+6r3ad@users.noreply.github.com>
2024-12-11 15:07:14 -0500

    Update client.ts

--- src/Kanbas/Courses/Quizzes/Questions/client.ts
+++ src/Kanbas/Courses/Quizzes/Questions/client.ts
@@ -1,12 +1,23 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const QUESTIONS_API = `${REMOTE_SERVER}/api/questions`;
-export const deleteQuestion = async (quizId: string) => {
+
+export const fetchQuestions = async (quiz: any) => {
+    const { data } = await axios.get(`${QUESTIONS_API}/${quiz}`);
+    return data;
+}
+
+export const deleteQuestion = async (question: string) => {
     // create new database collection for questions maybe
     const response = await axios.delete(`${QUESTIONS_API}/${question._id}`);
     return response.data;
 };
-export const updateQuestion = async (quiz: any) => {
+export const createQuestion = async (question: any) => {
+    const { data } = await axios.post(`${QUESTIONS_API}/${question._id}`, question);
+    return data;
+};
+
+export const updateQuestion = async (question: any) => {
     const { data } = await axios.put(`${QUESTIONS_API}/${question._id}`, question);
     return data;
 };
